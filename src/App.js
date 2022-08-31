1c6784c2c7ac3c598eee6ed220bde54f1da30258
Myudro33 <qanashvili.myudro@gmail.com>
2022-08-31 17:22:17 +0400

    set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{fontFamily:'helvetica'}}>
+        ჩანაწერის დამატება
+      </h1>
+      </div>
+  )
+}
+
+export default App
